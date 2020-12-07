9d4b9b6e4d281676a3650a59a35c58e7e7d6abf0
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-07 10:43:23 -0800

    Added additional NPM variables, and a connection route

--- employeeTrack.sql
+++ employeeTrack.sql
@@ -1,8 +1,8 @@
 -- ============COMMAND TO CREATE A DATABASE AND REMOVE A DATABASE OF THE SAME NAME========== --
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE employee_trackerDB;
+DROP DATABASE IF EXISTS employee_trackerdb;
+CREATE DATABASE employee_trackerdb;
 -- ============COMMAND TO USE THE PREVIOUSLY CREATED DATABASE==============
-USE employee_trackerDB;
+USE employee_trackerdb;
 -- =============CREATE A TABLE TO HOLD THE DEPARTMENT==================
 CREATE TABLE department (
     id INT PRIMARY KEY,
--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const sequelize = require("sequelize");
+//=============DECLARE THE CONNECTION TO INITIATE THE SQL DATABASE================//
+var connection = mysql.createConnection({
+    host:"localhost",
+    //======PORT NUMBER=======//
+    port: 3306,
+    //=========USERNAME========//
+    user:"root",
+    //========PASSWORD & DATABASE BEING USED=========//
+    password: "J0rd4n@(*&^%$#2814",
+    database: "employee_trackerdb"
+});
