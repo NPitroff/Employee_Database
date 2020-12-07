cb9baa5e889dfe0a879d31a2d55df712cd297539
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-07 10:20:44 -0800

    Added a 'server.js' file to initiate the commands

---
+++ server.js
@@ -0,0 +1,5 @@
+// ============DECLARE THE NPM PACKAGES USED IN THE APPLICATION ================//
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const sequelize = require("sequelize");
