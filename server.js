0586940ac00a90397a94b184a949d0e0d5851b9d
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-07 14:58:47 -0800

    Added Path for 'Adding Department'

--- server.js
+++ server.js
@@ -5,47 +5,68 @@ const consoleTable = require("console.table");
 const sequelize = require("sequelize");
 //=============DECLARE THE CONNECTION TO INITIATE THE SQL DATABASE================//
 var connection = mysql.createConnection({
-    host:"localhost",
-    //======PORT NUMBER=======//
-    port: 3306,
-    //=========USERNAME========//
-    user:"root",
-    //========PASSWORD & DATABASE BEING USED=========//
-    password: "J0rd4n@(*&^%$#2814",
-    database: "employee_trackerdb"
+  host: "localhost",
+  //======PORT NUMBER=======//
+  port: 3306,
+  //=========USERNAME========//
+  user: "root",
+  //========PASSWORD & DATABASE BEING USED=========//
+  password: "J0rd4n@(*&^%$#2814",
+  database: "employee_trackerdb",
 });
 //===============COMMAND TO THROW ERROR OR INITIATE INQUIRER UPON STARTUP============//
-connection.connect(function(err){
-    if (err) throw err;
-    runSearch();
+connection.connect(function (err) {
+  if (err) throw err;
+  runSearch();
 });
 //===============INQUIRER COMMAND PATH QUESTION 1====================//
-function runSearch(){
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "GREETINGS! WHAT WOULD YOU LIKE TO DO?",
+      choices: [
+        "ADD Department, Role, and/or Employee",
+        "VIEW CURRENT Department, Role, and/or Employee's",
+        "UPDATE A(n) Department, Role, and/or Employee",
+      ],
+    })
+    //==============PATH TO THE 'ADD' SELECTOR============//
+    .then(function (answer) {
+      switch (answer.action) {
+        case "ADD Department, Role, and/or Employee":
+          addInformation();
+          break;
+        //===============PATH TO THE 'VIEW' SELECTOR==========//
+        case "VIEW CURRENT Department, Role, and/or Employee's":
+          viewInformation();
+          break;
+        //===============PATH TO THE 'UPDATE' SELECTOR========//
+        case "UPDATE A(n) Department, Role, and/or Employee":
+          updateInformation();
+          break;
+      }
+    });
+}
+// ====================FUNCTIONS CALLED IN THE PATHS=============//
+function addInformation() {
     inquirer
     .prompt({
-        name:"action",
+        name: "action",
         type: "rawlist",
-        message: "GREETINGS! WHAT WOULD YOU LIKE TO DO?",
+        Message: "WHAT INFORMATION WOULD YOU LIKE TO ADD?",
         choices:[
-            "ADD Department, Role, and/or Employee",
-            "VIEW CURRENT Department, Role, and/or Employee's",
-            "UPDATE A(n) Department, Role, and/or Employee"
+            "ADD department",
+            "ADD role",
+            "ADD employee"
         ]
     })
-//==============PATH TO THE 'ADD' SELECTOR============//
-.then(function(answer){
-    switch (answer.action){
-    case "ADD Department, Role, and/or Employee":
-        addInformation();
-        break;
-//===============PATH TO THE 'VIEW' SELECTOR==========//
-case  "VIEW CURRENT Department, Role, and/or Employee's":
-        viewInformation();
-        break;
-//===============PATH TO THE 'UPDATE' SELECTOR========//
-case "UPDATE A(n) Department, Role, and/or Employee":
-        updateInformation();
-        break;
-}
-})
+    .then(function(answer){
+        switch (answer.action){
+            case "ADD department":
+                addDepartment();
+                break;
+        }
+    })
 }
